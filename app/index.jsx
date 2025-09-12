7a48f4e6e15ab733e9c2f92b63d7b7ee34cd39d4
vinicius.nogueira <vinicius.nogueira@unidesk.com.br>
2025-09-12 15:36:56 -0300

    alteração do background e cor do texto

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.jsx to edit this screen!!</Text>
+    <View style={my_styles.container}>
+      <Text style={my_styles.text}>Edit app/index.jsx to edit this screen!!</Text>
     </View>
   );
 }
+
+const my_styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#021123",
+  },
+  text: {
+    color: "#FFF",
+  },
+});
