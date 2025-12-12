a53d74101e93da9de7647e0671107b1080a8e173
vinicius.unidesk <vinicius.nogueira@unidesk.com.br>
2025-12-12 15:17:42 -0300

    Estilo e componentes pressable

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,27 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Image, Pressable, StyleSheet, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={myStyle.container}>
       <Image source={require("./pomodoro.png")}/>
-      <View style={myStyle.actions}/>
+      <View style={myStyle.actions}>
+        <Text style={myStyle.timer}>
+          25:00
+        </Text>
+          <Pressable style={myStyle.button}>
+            <Text style={myStyle.buttonText}>
+              Começar
+            </Text>
+          </Pressable>
+      </View>
+      <View style={myStyle.footer}>
+        <Text style={myStyle.footerText}>
+          Projeto aprendizado de 
+        </Text>
+        <Text style={myStyle.footerText}>
+          Vinicius Nogueira
+        </Text>
+      </View>
     </View>
   );
 }
@@ -24,6 +41,30 @@ const myStyle = StyleSheet.create({
       width: "80%",
       borderRadius: 32,
       borderWidth: 2,
-      borderColor: "#144480"
+      borderColor: "#144480",
+      gap: 32
+  },
+  timer : {
+    fontSize: 54,
+    color:'#FFFFFF',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  button:{
+    backgroundColor: '#B872FF',
+    borderRadius: 32,
+    padding: 8,
+  },
+  buttonText:{
+    textAlign: 'center',
+    color: '#021123',
+    fontSize: 18     
+  },
+  footer:{
+    width: '80%'
+  },
+  footerText:{
+    textAlign: 'center',
+    color: '#98A0A8'    
   }
 });
